b106abf7dd759dfc8c7edbfc26a56c8c3736ea24
2_donghh <rirws96@naver.com>
2024-05-30 14:29:56 +0900

    1543번 제출

--- src/stirng/no1543.java
+++ src/stirng/no1543.java
@@ -1,2 +1,25 @@
-package stirng;public class no1543 {
+package stirng;
+
+
+import java.util.Scanner;
+
+public class no1543 {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            // 사용자로부터 입력
+            String str1 = sc.nextLine();
+            String str2 = sc.nextLine();
+
+            int count = 0;
+            int index = 0;
+
+            while ((index = str1.indexOf(str2, index)) != -1) {
+                count++;
+                index += str2.length();
+            }
+
+            System.out.println(count);
+        }
 }
