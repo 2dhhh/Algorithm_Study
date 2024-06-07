f907d621f63178a70b3e441659f85313028462d2
2_donghh <rirws96@naver.com>
2024-06-07 13:05:35 +0900

    10158번 제출

---
+++ src/big_o/no10158.java
@@ -0,0 +1,28 @@
+package big_o;
+
+import java.util.Scanner;
+
+public class no10158 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+        int p = sc.nextInt();
+        int q = sc.nextInt();
+        int t = sc.nextInt();
+
+        int currentX = (p+t) % (2*w);
+        int currentY = (q+t) % (2*h);
+
+        if(currentX > w) currentX = 2*w - currentX;
+        if(currentY > h) currentY = 2*h - currentY;
+
+        System.out.println(currentX+" "+currentY);
+
+
+    }
+
+}
