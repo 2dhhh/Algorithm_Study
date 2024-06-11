6e7372f2037ef2d69153e84cbe3400fc2812f059
2_donghh <rirws96@naver.com>
2024-06-11 12:32:50 +0900

    1000번 제출

---
+++ src/big_o/no1000.java
@@ -0,0 +1,18 @@
+package big_o;
+
+
+import java.util.Scanner;
+
+public class no1000 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A+B);
+
+    }
+}
