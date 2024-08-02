44894ce9b60e7c47bdd35422d75e4071e3e49c83
이동혁 <idonghyeog@2donghh-MacBookAir.local>
2024-08-02 16:37:21 +0900

    11068번 제출

--- src/stirng/no1543.java
+++ src/stirng/no1543.java
@@ -3,23 +3,6 @@ import java.io.*;
 
 public class no1543 {
 
-//        public static void main(String[] args) {
-//
-//            Scanner sc = new Scanner(System.in);
-//            // 사용자로부터 입력
-//            String str1 = sc.nextLine();
-//            String str2 = sc.nextLine();
-//
-//            int count = 0;
-//            int index = 0;
-//
-//            while ((index = str1.indexOf(str2, index)) != -1) {
-//                count++;
-//                index += str2.length();
-//            }
-//
-//            System.out.println(count);
-//        }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
