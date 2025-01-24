a71fe5bdc35674a5516ee7883116d86247168919
이동혁 <rirws96@naver.com>
2025-01-24 16:33:24 +0900

    baekjoon: 누적합_14453제출

--- baekjoon/누적 합/No14453.py
+++ baekjoon/누적 합/No14453.py
@@ -1,44 +1,40 @@
-# H > S, S > P, P > H
 import sys
 
 input = sys.stdin.readline
 
-N = int(input().rstrip())
+n = int(input())
 
-H = [0] * (N + 1)
-P = [0] * (N + 1)
-S = [0] * (N + 1)
+hoof = [0] * (n+1)
+scissors = [0] * (n+1)
+paper = [0] * (n+1)
 
-array = [0]
-for i in range(N):
-  array.append(input().rstrip())
+for i in range(1,n+1):
+  str = input().rstrip()
 
-for i in range(1, N + 1):
+  if str == 'H' :
+    hoof[i] = hoof[i-1] +1
+    scissors[i] = scissors[i-1]
+    paper[i] = paper[i-1]
+  elif str == 'S' :
+    hoof[i] = hoof[i-1]
+    scissors[i] = scissors[i-1] +1
+    paper[i] = paper[i-1]
 
-  if array[i] == 'H':
-    H[i] = H[i - 1] + 1
-    P[i] = P[i - 1]
-    S[i] = S[i - 1]
+  else :
+    hoof[i] = hoof[i-1]
+    scissors[i] = scissors[i-1]
+    paper[i] = paper[i-1] +1
 
-  elif array[i] == 'P':
-    H[i] = H[i - 1]
-    P[i] = P[i - 1] + 1
-    S[i] = S[i - 1]
+ans = 0
+for i in range(1, n+1):
+  h_s = hoof[i-1] + (scissors[n] - scissors[i-1])
+  h_p = hoof[i-1] + (paper[n] - paper[i-1])
+  s_h = scissors[i-1] + (hoof[n] - hoof[i-1])
+  s_p = scissors[i-1] + (paper[n] - paper[i-1])
+  p_h = paper[i-1] + (hoof[n] - hoof[i-1])
+  p_s = paper[i-1] + (scissors[n] - scissors[i-1])
 
-  elif array[i] == 'S':
-    H[i] = H[i - 1]
-    P[i] = P[i - 1]
-    S[i] = S[i - 1] + 1
+  ans = max(h_s, h_p, s_h, s_p, p_h, p_s,ans)
 
-result = 0
-for i in range(1, N + 1):
-  p_h = P[i - 1] + (H[N] - H[i - 1])
-  p_s = P[i - 1] + (S[N] - S[i - 1])
-  h_s = H[i - 1] + (S[N] - S[i - 1])
-  h_p = H[i - 1] + (P[N] - P[i - 1])
-  s_p = S[i - 1] + (P[N] - P[i - 1])
-  s_h = S[i - 1] + (H[N] - H[i - 1])
+print(ans)
 
-  result = max(result, p_h, p_s, h_s, h_p, s_p, s_h)
-
-print(result)
