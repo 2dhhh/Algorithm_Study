c4adcf531d062d41c141f9acd6e163e175a4d03d
이동혁 <rirws96@naver.com>
2025-07-04 15:18:35 +0900

    baekjoon: 백트래킹_15650 풀이 추가

--- 백준/백트래킹/No15650.py
+++ 백준/백트래킹/No15650.py
@@ -2,22 +2,38 @@ import sys
 
 input = sys.stdin.readline
 
-def recur(k, cnt):
-  # 배열의 끝
-  if k == n + 1:
-    if cnt == m:
-      print(*selected)
+def recur(depth, start):
+  if depth == m:
+    print(*selected)
     return
-
-  else:
-    selected.append(k)
-    # 고를래?
-    recur(k + 1, cnt + 1)
-    selected.pop()
-    # 말래 ?
-    recur(k + 1, cnt)
-
+  for i in range(start, n + 1):
+    selected[depth] = i
+    recur(depth + 1, i + 1)
 
 n, m = map(int, input().split())
-selected = []
-recur(1, 0)
+selected = [0] * m
+recur(0, 1)
+
+# import sys
+#
+# input = sys.stdin.readline
+#
+# def recur(k, cnt):
+#   # 배열의 끝
+#   if k == n + 1:
+#     if cnt == m:
+#       print(*selected)
+#     return
+#
+#   else:
+#     selected.append(k)
+#     # 고를래?
+#     recur(k + 1, cnt + 1)
+#     selected.pop()
+#     # 말래 ?
+#     recur(k + 1, cnt)
+#
+#
+# n, m = map(int, input().split())
+# selected = []
+# recur(1, 0)
