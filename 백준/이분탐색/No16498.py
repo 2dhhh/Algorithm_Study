3aae2948c22dcc3de3319ae14a1c6ce46c1b9d77
이동혁 <rirws96@naver.com>
2025-07-24 17:27:48 +0900

    baekjoon: 이분탐색_16498 코드 수정

--- 백준/이분탐색/No16498.py
+++ 백준/이분탐색/No16498.py
@@ -3,27 +3,32 @@ import sys
 input = sys.stdin.readline
 
 a, b, c = map(int, input().split())
-ls_a = list(map(int,input().split()))
-ls_b = list(map(int,input().split()))
-ls_c = sorted(list(map(int,input().split())))
-ans = int(1e10)
-for A in ls_a:
-  for B in ls_b:
-    max_v = max(A,B)
-    min_v = min(A,B)
-    s ,e = 0, c-1
+# A 카드
+ls_a = list(map(int, input().split()))
+# B 카드
+ls_b = list(map(int, input().split()))
+# C 카드
+ls_c = sorted(list(map(int, input().split())))
+
+ans = int(1e9)
+
+for i in ls_a:
+  for j in ls_b:
+    s, e = 0, c - 1
+    r = max(i, j)
+    l = min(i, j)
     while s <= e:
-      mid = (s+e) // 2
-      if min_v <= ls_c[mid] <= max_v:
-        ans = min(ans, max_v - min_v)
+      mid = (s + e) // 2
+      if l <= ls_c[mid] <= r:
+        ans = min(ans, r - l)
         break
 
-      elif ls_c[mid] > max_v:
-        ans = min(ans, ls_c[mid] - min_v)
-        e = mid -1
+      elif ls_c[mid] > r:
+        ans = min(ans, ls_c[mid] - l)
+        e = mid - 1
 
       else:
-        ans = min(ans, max_v - ls_c[mid])
+        ans = min(ans, r - ls_c[mid])
         s = mid + 1
 
 print(ans)
