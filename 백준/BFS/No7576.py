4fb091f96781be44179982a1536f43d650a52c14
이동혁 <rirws96@naver.com>
2025-09-09 11:52:52 +0900

    baekjoon: BFS_7576 풀이수정

--- 백준/BFS/No7576.py
+++ 백준/BFS/No7576.py
@@ -1,40 +1,42 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-dxy = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-m, n = map(int, input().split())
+
+dxy = [(-1, 0), (1, 0) , (0, 1), (0, -1)]
+m, n = map(int ,input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
-cnt = 0
+visited = [[False] * m for _ in range(n)]
 q = deque()
+day = 0
+# 익은 토마토 큐에 삽입
 for i in range(n):
   for j in range(m):
     if board[i][j] == 1:
-      q.append((i,j))
-      cnt += 1
-    if cnt == (n + 1) * (m + 1):
-      print(0)
-      exit()
+      q.append((i, j))
 
-dist = 0
+
 while q:
-  sz =len(q)
+  sz = len(q)
 
   for _ in range(sz):
     x, y = q.popleft()
     for dx, dy in dxy:
-      nx, ny = x + dx, y + dy
+      nx ,ny = x + dx, y + dy
+      # 범위 체크
       if nx < 0 or nx >= n or ny < 0 or ny >= m:
         continue
+      # 토마토가 없는 경우 혹은 이미 익은 경우
       if board[nx][ny] != 0:
         continue
-      board[nx][ny] = 1
       q.append((nx, ny))
-  dist += 1
+      board[nx][ny] = 1
+
+  day += 1
 
 for i in range(n):
   for j in range(m):
     if board[i][j] == 0:
       print(-1)
-      exit()
+      exit(0)
 
-print(dist -1)
+print(day - 1)
