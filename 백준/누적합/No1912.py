208721e243d88b271b56b3032da065abec2c856a
이동혁 <rirws96@naver.com>
2025-09-10 16:14:14 +0900

    baekjoon: 누적합_1912 풀이 수정

--- 백준/누적합/No1912.py
+++ 백준/누적합/No1912.py
@@ -8,29 +8,17 @@ ls = [0] + list(map(int, input().split()))
 
 prefix_sum = [0] * (n+1)
 
-max_value = -1000
+for i in range(1, n+1):
+  prefix_sum[i] = prefix_sum[i-1] + ls[i]
+
+max_value = -int(1e8)
+# 최소값 기준을 0으로 세팅하는게 핵심
 min_value = 0
 
-
-
-# 누적 힙 배열
 for i in range(1, n+1):
-  prefix_sum[i] = prefix_sum[i-1] + ls[i] # ex [0, 10, 6, 9, 10, 15, 21, -14, -2, 19]
 
-
-for i in range(1, n+1):
   max_value = max(max_value, prefix_sum[i] - min_value)
 
-  if min_value > prefix_sum[i]:
-    min_value = prefix_sum[i]
+  min_value = min(min_value, prefix_sum[i])
 
 print(max_value)
-
-
-
-
-
-
-
-
-
